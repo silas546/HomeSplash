99415f581f9db9f40a997ab2eeaf7c6efca5b32c
Silas Owings <silasowings@gmail.com>
2015-03-02 20:38:41 -0500

    added clock and greeting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @current_time = Time.now.to_formatted_s(:short)
+    # homework - implement stamp gem
+    @greet_message = case Time.now.hour
+    when 5..11
+      "Good Morning"
+    when 12..15
+      "Good Afternoon"
+    when 16..19
+      ["Buenas Afternoonas", "Good Afternoon", "Howdy"].sample
+    else
+      "Gnight!"
+    end
   end
 end
